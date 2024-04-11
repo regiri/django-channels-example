6f6cf861ee0b4aeec0b1d479d70562196035f0df
regiri <goparegiri@gmail.com>
2024-04-11 17:55:01 +0300

    feat: create django-channels example project

---
+++ my_site/chat/views.py
@@ -0,0 +1,15 @@
+from django.views import generic
+
+
+class IndexView(generic.TemplateView):
+    template_name = "chat/index.html"
+
+
+class RoomView(generic.TemplateView):
+    template_name = "chat/room.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["room_name"] = self.kwargs["room_name"]
+        return context
+    
